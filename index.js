e1cdb75521a0e594376d057b753fb1801641e461
Tim Brandau <96590473+tacticalbison@users.noreply.github.com>
2022-09-21 09:36:04 -0500

    up to lesson 164.

---
+++ index.js
@@ -0,0 +1,24 @@
+let buttons = document.querySelectorAll("button");
+
+buttons.forEach((ele) => {
+  ele.addEventListener("click", handleClick);
+});
+
+let crash = new Audio("../sounds/crash.mp3");
+let kick = new Audio("../sounds/kick-bass.mp3");
+let snare = new Audio("../sounds/snare.mp3");
+let tom1 = new Audio("../sounds/tom-1.mp3");
+let tom2 = new Audio("../sounds/tom-2.mp3");
+let tom3 = new Audio("../sounds/tom-3.mp3");
+let tom4 = new Audio("../sounds/tom-4.mp3");
+
+function handleClick() {
+  this.style.color = "white";
+
+  if (this.className.split(" ").includes("drum")) {
+    tom2.play();
+  }
+  setTimeout(() => {
+    this.style.color = "";
+  }, 1000);
+}
